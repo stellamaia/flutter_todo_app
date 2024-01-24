907eb58e9a07d094f098f2b45a0808c344d8b307
stella <stellamaia97@hotmail.com>
2024-01-23 23:42:24 -0300

    Análise de Sentimentos

--- lib/exercises/exercise-screen.dart
+++ lib/exercises/exercise-screen.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/models/exercises-model.dart';
+import 'package:flutter_todo_app/models/sentiment-model.dart';
 
 class ExerciseScreen extends StatelessWidget {
-  const ExerciseScreen({super.key});
+  ExerciseScreen({super.key});
 
+  final ExercisesModels exercisesModels = ExercisesModels(
+    id: "EX001",
+    name: "Remada Baixa Supinada",
+    training: "Treino A",
+    howToMake: "Segura a barra e puxa",
+  );
+
+  final List<SentimentModel> sentiment = [
+    SentimentModel(
+      id: "EX001",
+      sentiment: "Pouca ativação hoje",
+      data: "2022-02-17",
+    ),
+    SentimentModel(
+      id: "EX001",
+      sentiment: "Já senti alguma ativação",
+      data: "2022-02-19",
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Puxada Alta Pronada - Treino A ")),
+      appBar: AppBar(
+        title: Text(
+          "${exercisesModels.name} - ${exercisesModels.training}",
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           //comportamento de quando foi clicado
@@ -21,14 +46,19 @@ class ExerciseScreen extends StatelessWidget {
           children: [
             ElevatedButton(
               onPressed: () {},
-              child: const Text("Enviar foto"),
+              child: const Text(
+                "Enviar foto",
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue, // background color
+              ),
             ),
             const Text(
               "Como fazer?",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
             ),
-            const Text(
-                "Segura com as duas mãos na barra, mantém a coluna reta e puxa"),
+            Text(exercisesModels.howToMake),
             const Divider(),
             const Text(
               "Como estou me sentindo?",
